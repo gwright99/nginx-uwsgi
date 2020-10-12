17595a9a9d3f447c7e0721809fb499f41099d8aa
Graham Wright <gwright99@hotmail.com>
2020-10-12 14:15:47 -0400

    Found typos in uwsgi.ini and was missing static file

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,12 +1,26 @@
-from flask import Flask
-
-application = Flask(__name__)
+from flask import Flask, request, jsonify
 
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    application.run()
+@app.route('/')
+def hello_world():
+    return """
+        Simple response from Flask running inside uWSGI, 
+        which is running within nGINX, 
+        which is hosted inside a Docker image!
+    """
+
+
+@app.route('/echo_request')
+def echo_request():
+    return jsonify(dict(request.headers))
+
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=8080
+    )
